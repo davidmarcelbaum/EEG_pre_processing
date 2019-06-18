54be5910e35c5dc7f42181a4e88e61f245d222e5
David Marcel BAUM <david.baum@tuta.io>
2019-06-18 11:08:52 -0400

    Small change

--- SubsequentICA.m
+++ SubsequentICA.m
@@ -1,4 +1,4 @@
-%Search for EEGLAB functions folder
+%Search for EEGLAB folder
 locateEeglab = which('eeglab.m');
 eeglabFolder = erase(locateEeglab, "eeglab.m");
 
@@ -11,7 +11,7 @@ strVerify = strfind(strSystem,'PCWIN');
 if isempty(locateEeglab)
     %Point to functions used in this sript
     functionsEEGLAB = uigetdir(matlabroot,'Point to the folder >>functions<< of EEGLAB');
-    
+
     if isempty(strVerify)
         addpath = strcat(functionsEEGLAB, '/', 'adminfunc', '/');
         addpath = strcat(functionsEEGLAB, '/', 'popfunc', '/');
@@ -42,7 +42,7 @@ EEG = [];
 
 %if >1 files selected FilesList is a cell array, if 1 file only, then FilesList is a char --> transform to cell array
 if ischar(FilesList)
-    FilesList = {FilesList}; 
+    FilesList = {FilesList};
 end
 
 %Creates ICAWeights folder if does not exist. This is essential for saving
@@ -54,60 +54,60 @@ if existsICAWeights ~= 7
 end
 
 for Filenum = 1:numel(FilesList) %Loop going from the 1st element in the folder, to the total elements
-    
+
     %Function to get the file name
     fileNameComplete = char(FilesList(Filenum));
     fileName = fileNameComplete(1:21);
-    
+
     %Append ICAWeights to file names and set path for future save to
     %"daughter folder" ICAClean
     newFileName = strcat(fileName, '_ICAWeights.set');
-    
+
     if isempty(strVerify)
         newFilePath = strcat(pathName, 'ICAWeights/');
     else
         newFilePath = strcat(pathName, 'ICAWeights\');
     end
-    
+
     %Check if dataset has already been run ICA on
     existsFile = exist ([newFilePath, newFileName], 'file');
-    
+
     if existsFile ~= 2
-        
+
         %Function to load .set into EEGLAB
         EEG = pop_loadset('filename',fileNameComplete,'filepath',pathName);
-    
+
         %Don't know what this is doing, but I guess it is saving the opened
         %dataset in current and a global dataset
         [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0 );
         EEG = eeg_checkset( EEG );
-        
+
         %Look if dataset contains Trigger channel
         searchTrigger = strfind(strcat(EEG.chanlocs.labels), 'Trigger');
-        
+
         if isempty(searchTrigger)
            ChannelsICA = EEG.nbchan;
         else
-           ChannelsICA = EEG.nbchan-1; 
+           ChannelsICA = EEG.nbchan-1;
         end
-        
+
         %Function to run ICA with specific parameters
         EEG = pop_runica(EEG, 'icatype', 'runica', 'extended',1,'interrupt','off','chanind',1:ChannelsICA);
         EEG = eeg_checkset( EEG );
-    
+
         %Append "ICAWeights" to filename
         EEG = pop_editset(EEG, 'setname', newFileName);
         EEG = eeg_checkset( EEG );
-    
+
         %Saving new file name to new path
         EEG = pop_saveset( EEG, 'filename',newFileName,'filepath',newFilePath,'savemode','onefile');
         EEG = eeg_checkset( EEG );
-    
+
         %Purge dataset from memory
         [ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
         EEG = pop_delset( EEG, [1] );
     end
-    
+
 end
 
 %Display end message

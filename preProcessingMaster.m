15662ea7a6f616d170403977a296fc91c7b8f5e4
David Marcel BAUM <51487830+davidmarcelbaum@users.noreply.github.com>
2019-07-17 17:16:20 -0400

    Fix folder paths

--- preProcessingMaster.m
+++ preProcessingMaster.m
@@ -111,10 +111,10 @@ FilesList = {};
 %will put subfolders in existing preProcessing folder, digging into the folder paths
 %too deep and coming out on the other end of Earth: "Buongiorno!".
 if contains(pathName, 'preProcessing')
-    preProcessingFolder = replace(pathName,extractAfter(pathName,"preProcessing"),slashSys);
+    %preProcessingFolder = replace(pathName,extractAfter(pathName,"preProcessing"),slashSys);
+    preProcessingFolder = pathName;
 else
-    existsPreProcessing = exist([pathName, 'preProcessing'], 'dir');
-    if existsPreProcessing ~= 7
+    if exist([pathName, 'preProcessing'], 'dir') ~= 7
         mkdir (pathName, 'preProcessing');
     end
     preProcessingFolder = strcat(pathName, 'preProcessing', slashSys);

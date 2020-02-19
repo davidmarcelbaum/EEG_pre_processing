58776953c25e6f839c5aadb289d490edf96b51b1
davidmarcelbaum <david.baumtuta.io>
2020-02-19 18:48:09 -0300

    Introduce .cdt file handling and eliminate code redundancy

--- prerequisities.m
+++ prerequisities.m
@@ -4,26 +4,21 @@
 
 %Since the path to EEGLAB differs between systems, locate EEGLAB and its functions.
 locateEeglab = which('eeglab.m');
-eeglabFolder = erase(locateEeglab, 'eeglab.m');
 
-%Determines which system MAtlab runs on and set slash accordingly since it differs Windows vs
-%Unix-like. The slash is used a lot afterwards, so best to define here.
-if contains(computer,'PCWIN') == 1
-    slashSys = '\';
-else
-    slashSys = '/';
-end
-
-if isempty(locateEeglab)
-    functionsEEGLAB = uigetdir(matlabroot,'Point to the folder "functions" of EEGLAB');
+if ~isempty(locateEeglab)
     
-    addpath(strcat(functionsEEGLAB, slashSys, 'adminfunc', slashSys));
-    addpath(strcat(functionsEEGLAB, slashSys, 'popfunc', slashSys));
+    [folderEEGLAB, ~, ~] = fileparts(locateEeglab);
+    
+    strcat(folderEEGLAB, filesep, 'functions', filesep, 'adminfunc');
+    strcat(folderEEGLAB, filesep, 'functions', filesep, 'popfunc');
+    strcat(folderEEGLAB, filesep, 'loadcurry2.0');
     
 else
-    addpath(strcat(eeglabFolder, 'functions', slashSys,'popfunc', slashSys));
-    addpath(strcat(eeglabFolder, 'functions', slashSys,'adminfunc', slashSys));
+    
+    error('EEGLAB not found')
+    
 end
+
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -34,41 +29,48 @@ end
 pathName = uigetdir(cd,'Choose the folder that contains the datasets');
 %[filesPreProcess, pathName] = uigetfile(cd,'Choose one dataset to build pathways','*.*','multiselect','off');
 
-pathName = strcat(pathName, slashSys);
+pathName = strcat(pathName, filesep);
 
 %This will deselect steps of the script that are not needed.
-chooseScriptParts = {'RAWing, Filtering and/or re-referencing','Interpolation of noisy channels','ICA','Epoching','Extract channel interpolation information','Compute dipoles with Nonlinear least-square fit regression curve','Organize Triggers','Reject empty channels','Transform chanlocs.xyz to .elc','Asign dipoles to atlas area'};
+chooseScriptParts = {'RAWing, Filtering and/or re-referencing',...
+    'Interpolation of noisy channels','ICA','Epoching',...
+    'Extract channel interpolation information',...
+    'Compute dipoles with Nonlinear least-square fit regression curve',...
+    'Organize Triggers','Reject empty channels',...
+    'Transform chanlocs.xyz to .elc','Asign dipoles to atlas area'};
 
-[scriptPart,tfParts] = listdlg('PromptString','What type of pre-processing do you want to perform?','SelectionMode','single','ListSize',[800,300],'ListString',chooseScriptParts);
+[scriptPart,tfParts] = listdlg('PromptString',...
+    'What type of pre-processing do you want to perform?',...
+    'SelectionMode','single','ListSize',[800,300],...
+    'ListString',chooseScriptParts);
 
 %Based on what the user chooses, the import and load functions of the datasets are impacted
-switch scriptPart
-    case 1
-        chooseFileFormat = {'.set','.mff folders'}; %DO NOT CHANGE ORDER OF FORMATS, adding is ok, though !!!
-        [fileFormat,tfFiles] = listdlg('PromptString','What is the format of your datasets?','SelectionMode','single','ListSize',[150,150],'ListString',chooseFileFormat);
-        fileFormatTranslation = fileFormat;
-    otherwise
-        fileFormat = 1;
-        fileFormatTranslation = fileFormat;
+
+chooseFileFormat = {'.set', '.mff', '.cdt'};
+
+if scriptPart == 1
+    [fileFormat,~] = listdlg('PromptString',...
+        'What is the format of your datasets?',...
+        'SelectionMode','single','ListSize',[150,150],...
+        'ListString',chooseFileFormat);
+    
+    fileFormat = chooseFileFormat(fileFormat);
+    
+else
+    fileFormat = '.set';
 end
 
-%Translate the indices of the fileFormat back to strings. This string is
-%used to search for files in the folder that match the file format. For
-%convenience in later parts of the script, I created a new variable
-%"fileFormatTranslation".
-switch fileFormatTranslation
-    case 1
-        fileFormatTranslation = '.set';
-    case 2
-        fileFormatTranslation = '.mff';
-end
 
 %Extract the base name structure of each dataset in order to later append extensions during saving
 dataList = dir(pathName);
-dataMatch = find(contains({dataList.name}, fileFormatTranslation));
+dataFormat = find(contains({dataList.name}, fileFormat));
 
-conservedCharacters = strlength(inputdlg({'Delete parts of file name that are not part of base name structure (Delete last underscore if there)'},...
-    'Base name structure', 1, cellstr(dataList(dataMatch(1)).name)));
+% Some header files have the same data format as files of interest, but
+% contain an additional format (ie .cdt.dpa). This line rejects these
+idxWrongFormat = contains(extractAfter({dataList(dataFormat).name}, ...
+    fileFormat), '.');
+
+dataFormat = dataFormat(~idxWrongFormat);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -76,12 +78,12 @@ conservedCharacters = strlength(inputdlg({'Delete parts of file name that are no
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Check if all information has been provided that will be used in the script
-if ~isempty(pathName) && ~isempty(fileFormat) && ~isempty(conservedCharacters) && ~isempty(scriptPart)
+if isempty(pathName) || isempty(fileFormat) || isempty(scriptPart)
     
-else
     %End the script if information is missing
     warning('Some information necessary for the script is missing! Run the script again and provide all information.')
     return
+    
 end
 
 %Matlab seems to scan script outside of if else conditions and complains
@@ -95,60 +97,55 @@ FilesList = {};
 %too deep and coming out on the other end of Earth: "Buongiorno!".
 if contains(pathName, 'preProcessing')
     %preProcessingFolder = replace(pathName,extractAfter(pathName,"preProcessing"),slashSys);
-    preProcessingFolder = replace(pathName, extractAfter(pathName, 'preProcessing'), slashSys);
+    preProcessingFolder = replace(pathName, extractAfter(pathName, 'preProcessing'), filesep);
 else
     if exist([pathName, 'preProcessing'], 'dir') ~= 7
         mkdir (pathName, 'preProcessing');
     end
-    preProcessingFolder = strcat(pathName, 'preProcessing', slashSys);
+    preProcessingFolder = strcat(pathName, 'preProcessing', filesep);
 end
 
 %Set up folder structures so that each step is saved in separate folder.
-folderRAW = strcat(preProcessingFolder, 'RAW', slashSys);
-folderFilt = strcat(preProcessingFolder, 'Filtered', slashSys);
-folderReference = strcat(preProcessingFolder, 'Re-reference', slashSys);
-folderChInterpol = strcat(preProcessingFolder, 'ChInterpol', slashSys);
-folderICAWeights = strcat(preProcessingFolder, 'ICAWeights', slashSys);
-folderICAClean = strcat(preProcessingFolder, 'ICAClean', slashSys);
-folderEpochs = strcat(preProcessingFolder, 'Epochs', slashSys);
-folderSelEpochs = strcat(preProcessingFolder, 'SelectedEpochs', slashSys);
-folderInterpolInfo = strcat(preProcessingFolder, 'ChannelInterpolation', slashSys);
-folderDipoles = strcat(preProcessingFolder, 'Dipoles', slashSys);
-folderOrganizeTriggers = strcat(preProcessingFolder, 'OrganizeTriggers', slashSys);
-folderRejEmptyChan = strcat(preProcessingFolder, 'RejEmptyChannel', slashSys);
-folderAtlas = strcat(preProcessingFolder, 'Atlas', slashSys);
+folderFiltRef = strcat(preProcessingFolder, 'FiltRef', filesep);
+folderChInterpol = strcat(preProcessingFolder, 'ChInterpol', filesep);
+folderICAWeights = strcat(preProcessingFolder, 'ICAWeights', filesep);
+folderICAClean = strcat(preProcessingFolder, 'ICAClean', filesep);
+folderEpochs = strcat(preProcessingFolder, 'Epochs', filesep);
+folderSelEpochs = strcat(preProcessingFolder, 'SelectedEpochs', filesep);
+folderInterpolInfo = strcat(preProcessingFolder, 'ChannelInterpolation', filesep);
+folderDipoles = strcat(preProcessingFolder, 'Dipoles', filesep);
+folderOrganizeTriggers = strcat(preProcessingFolder, 'OrganizeTriggers', filesep);
+folderRejEmptyChan = strcat(preProcessingFolder, 'RejEmptyChannel', filesep);
+folderAtlas = strcat(preProcessingFolder, 'Atlas', filesep);
 foldersCreated = {};
 
 %Set up initial stepLevel value so that later, pre-processing of datasets
 %is only forward and not reverse
 stepLevel = 0;
 
-RAWed = 1;
-Filtered = 2;
-Rereferenced = 3;
-ChInterpolated = 4;
-ICAWeighted = 5;
-ICACleaned = 6;
-Epoched = 7;
-SelEpoched = 8;
+FiltRef = 1;
+ChInterpolated = 2;
+SleepExtr = 3;
+ICAWeighted = 4;
+ICACleaned = 5;
+Epoched = 6;
+SelEpoched = 7;
 
 %Define stepLevel based on dataset names
 %dataMatch = find(contains({dataList.name}, fileFormatTranslation));
 %FilesList = dir([pathName,'*.*']);
-if contains(string(dataList(dataMatch(1)).name), '_RAW')
-    stepLevel = RAWed;
-elseif contains(string(dataList(dataMatch(1)).name), '_Filt')
-    stepLevel = Filtered;
-elseif contains(string(dataList(dataMatch(1)).name), '_Re-reference')
-    stepLevel = Rereferenced;
-elseif contains(string(dataList(dataMatch(1)).name), '_ChInterpol')
+if contains(string(dataList(dataFormat(1)).name), '_FiltRef')
+    stepLevel = FiltRef;
+elseif contains(string(dataList(dataFormat(1)).name), '_ChInterpol')
     stepLevel = ChInterpolated;
-elseif contains(string(dataList(dataMatch(1)).name), '_ICAWeights')
+elseif contains(string(dataList(dataFormat(1)).name), '_SleepExtr')
+    stepLevel = SleepExtr;
+elseif contains(string(dataList(dataFormat(1)).name), '_ICAWeights')
     stepLevel = ICAWeighted;
-elseif contains(string(dataList(dataMatch(1)).name), '_ICAClean')
+elseif contains(string(dataList(dataFormat(1)).name), '_ICAClean')
     stepLevel = ICACleaned;
-elseif contains(string(dataList(dataMatch(1)).name), '_Epochs')
+elseif contains(string(dataList(dataFormat(1)).name), '_Epochs')
     stepLevel = Epoched;
-elseif contains(string(dataList(dataMatch(1)).name), '_SelectedEpochs')
+elseif contains(string(dataList(dataFormat(1)).name), '_SelectedEpochs')
     stepLevel = SelEpoched;
 end
